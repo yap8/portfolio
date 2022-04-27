b176c0d3b756436032f1f03e91adda05d9a1088a
yap8 <nebozhenko.arseny@gmail.com>
2022-04-27 17:33:02 +0300

    configured container

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    container: {
+      padding: '1rem',
+      center: true,
+    },
   },
   plugins: [],
 };
