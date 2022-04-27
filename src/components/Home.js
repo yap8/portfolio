749fd9b4612bd2764f3286ea1c184ac04b52c09e
yap8 <nebozhenko.arseny@gmail.com>
2022-04-27 21:29:00 +0300

    added icons to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,6 @@
+import { AiOutlineGithub } from 'react-icons/ai';
+import { ImProfile } from 'react-icons/im';
+
 const Home = () => {
   return (
     <section id="home">
@@ -7,19 +10,21 @@ const Home = () => {
         <ul className="flex">
           <li className="mr-4">
             <a
-              className="block py-2 px-6 border border-blue-400 text-blue-400 rounded text-2xl transition hover:bg-blue-400 hover:text-slate-800"
+              className="flex items-center py-2 px-6 border border-blue-400 text-blue-400 rounded text-2xl transition hover:bg-blue-400 hover:text-slate-800"
               href="Resume.pdf"
             >
+              <ImProfile className="text-xl mr-2" />
               Resume
             </a>
           </li>
           <li>
             <a
-              className="block py-2 px-6 border border-blue-400 text-blue-400 rounded text-2xl transition hover:bg-blue-400 hover:text-slate-800"
+              className="flex items-center py-2 px-6 border border-blue-400 text-blue-400 rounded text-2xl transition hover:bg-blue-400 hover:text-slate-800"
               href="https://github.com/yap8"
               target="_blank"
               rel="noreferrer"
             >
+              <AiOutlineGithub className="text-2xl mr-2" />
               GitHub
             </a>
           </li>
