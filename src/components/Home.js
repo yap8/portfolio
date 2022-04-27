439801132217cdd917d4f58149e6eb0dca618887
yap8 <nebozhenko.arseny@gmail.com>
2022-04-27 17:29:54 +0300

    added Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,30 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section>
+      <div className="h-screen flex flex-col justify-center items-center">
+        <h1 className="text-5xl mb-4">Arseny Nebozhenko</h1>
+        <p className="text-xl text-gray-300 mb-6">Frontend Developer</p>
+        <ul className="flex">
+          <li className="mr-4">
+            <a
+              className="block py-2 px-6 border border-blue-400 text-blue-400 rounded text-2xl transition hover:bg-blue-400 hover:text-slate-800"
+              href="#"
+            >
+              Resume
+            </a>
+          </li>
+          <li>
+            <a
+              className="block py-2 px-6 border border-blue-400 text-blue-400 rounded text-2xl transition hover:bg-blue-400 hover:text-slate-800"
+              href="#"
+            >
+              GitHub
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
