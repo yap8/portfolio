393d5db5ca2f805eaa6198786c979ef3afba0781
yap8 <nebozhenko.arseny@gmail.com>
2022-04-27 20:11:16 +0300

    added Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,60 @@
+import { useState } from 'react';
+
 const Contact = () => {
-  return <div>Contact</div>;
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <section>
+      <div className="container py-20 flex flex-col justify-center items-center">
+        <h1 className="text-4xl text-blue-400 mb-10">Contact</h1>
+        <form
+          className="max-w-2xl w-full flex flex-col"
+          onSubmit={handleSubmit}
+        >
+          <input
+            className="mb-2 text-blue-400 text-2xl py-2 px-6 rounded bg-transparent border border-blue-400 placeholder:text-blue-400 focus:text-white"
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+          <input
+            className="mb-2 text-blue-400 text-2xl py-2 px-6 rounded bg-transparent border border-blue-400 placeholder:text-blue-400 focus:text-white"
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          <textarea
+            className="h-56 mb-2 text-blue-400 text-2xl py-2 px-6 rounded resize-none bg-transparent border border-blue-400 placeholder:text-blue-400 focus:text-white"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+          />
+          <button className="mr-auto py-2 px-6 border border-blue-400 text-blue-400 rounded text-2xl transition hover:bg-blue-400 hover:text-slate-800">
+            Send
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
