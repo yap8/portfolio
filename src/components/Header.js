212d6bef604d53ee6ad18faaa983d480eed9d0e2
yap8 <nebozhenko.arseny@gmail.com>
2022-04-27 20:54:41 +0300

    added icons to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,31 @@
+import { AiOutlineHome, AiOutlineMessage } from 'react-icons/ai';
+import { MdWorkOutline } from 'react-icons/md';
+import { BiBook } from 'react-icons/bi';
+
 const Header = () => {
   return (
     <header className="fixed bottom-8 w-full">
       <nav className="m-auto w-fit py-4 px-8 bg-slate-600 bg-opacity-60 backdrop-blur-sm rounded-full">
         <ul className="flex justify-center">
           <li className="mr-4">
-            <a href="#skills">skills</a>
+            <a href="#skills">
+              <AiOutlineHome />
+            </a>
           </li>
           <li className="mr-4">
-            <a href="#projects">projects</a>
+            <a href="#projects">
+              <BiBook />
+            </a>
+          </li>
+          <li className="mr-4">
+            <a href="#skills">
+              <MdWorkOutline />
+            </a>
           </li>
           <li>
-            <a href="#contact">contact</a>
+            <a href="#contact">
+              <AiOutlineMessage />
+            </a>
           </li>
         </ul>
       </nav>
