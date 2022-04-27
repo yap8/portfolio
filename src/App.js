e76f606e70de28ccf6e6d6a4ccb3c5affcdc0d6c
yap8 <nebozhenko.arseny@gmail.com>
2022-04-27 20:44:55 +0300

    added Header

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import 'react-toastify/dist/ReactToastify.css';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Skills from './components/Skills';
+import Header from './components/Header';
 import Home from './components/Home';
 
 const App = () => {
   return (
     <div>
       <ToastContainer position="top-center" />
+      <Header />
       <Home />
       <Skills />
       <Projects />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,21 @@
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="fixed bottom-8 w-full">
+      <nav className="m-auto w-fit py-4 px-8 bg-slate-600 bg-opacity-60 backdrop-blur-sm rounded-full">
+        <ul className="flex justify-center">
+          <li className="mr-4">
+            <a href="#skills">skills</a>
+          </li>
+          <li className="mr-4">
+            <a href="#projects">projects</a>
+          </li>
+          <li>
+            <a href="#contact">contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
