4f01818c324194efb0fd8b3b4c030a50a1cded5c
yap8 <nebozhenko.arseny@gmail.com>
2022-04-27 18:51:32 +0300

    fixed margin

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -28,8 +28,8 @@ const Projects = () => {
       <div className="container flex flex-col justify-center items-center">
         <h1 className="text-4xl text-blue-400 mb-10">Projects</h1>
         <ul className="flex justify-center w-full">
-          {projects.map((project) => (
-            <li className="flex flex-col justify-between flex-1 py-8 px-6 mr-8 bg-slate-900 rounded-lg">
+          {projects.map((project, index) => (
+            <li className="flex flex-col justify-between flex-1 py-8 px-6 mr-8 bg-slate-900 rounded-lg last:mr-0">
               <div className="h-80 overflow-hidden rounded-lg mb-4">
                 <img
                   style={{ minHeight: '100%', minWidth: '100%' }}
