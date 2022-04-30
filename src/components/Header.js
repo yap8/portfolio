3fe7b764564ac984c3895256971d9204c4db7ca9
yap8 <nebozhenko.arseny@gmail.com>
2022-04-30 21:54:22 +0300

    reworked Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,59 +3,32 @@ import { MdWorkOutline } from 'react-icons/md';
 import { Link } from 'react-scroll/modules';
 import { BiBook } from 'react-icons/bi';
 
+const links = [
+  { to: 'home', Icon: <AiOutlineHome /> },
+  { to: 'skills', Icon: <BiBook /> },
+  { to: 'projects', Icon: <MdWorkOutline /> },
+  { to: 'contact', Icon: <AiOutlineMessage /> },
+];
+
 const Header = () => {
   return (
     <header className="fixed inset-x-0 m-auto w-fit bottom-8">
       <nav className="py-4 px-8 bg-slate-600 bg-opacity-80 backdrop-blur-sm rounded-full shadow">
         <ul className="flex justify-center text-2xl text-white">
-          <li className="mr-4">
-            <Link
-              className="transition"
-              activeClass="text-blue-400"
-              to="home"
-              spy={true}
-              smooth={true}
-              duration={500}
-            >
-              <AiOutlineHome />
-            </Link>
-          </li>
-          <li className="mr-4">
-            <Link
-              className="transition"
-              activeClass="text-blue-400"
-              to="skills"
-              spy={true}
-              smooth={true}
-              duration={500}
-            >
-              <BiBook />
-            </Link>
-          </li>
-          <li className="mr-4">
-            <Link
-              className="transition"
-              activeClass="text-blue-400"
-              to="projects"
-              spy={true}
-              smooth={true}
-              duration={500}
-            >
-              <MdWorkOutline />
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="transition"
-              activeClass="text-blue-400"
-              to="contact"
-              spy={true}
-              smooth={true}
-              duration={500}
-            >
-              <AiOutlineMessage />
-            </Link>
-          </li>
+          {links.map(({ to, Icon }) => (
+            <li className="mr-4 last:mr-0 hover:text-blue-400" key={to}>
+              <Link
+                className="transition cursor-pointer"
+                activeClass="text-blue-400"
+                to={to}
+                spy={true}
+                smooth={true}
+                duration={500}
+              >
+                {Icon}
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
