47fb882468311d665c10c9c406aa9130cd9223cd
yap8 <nebozhenko.arseny@gmail.com>
2022-04-30 23:09:49 +0300

    reworked Skills

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,9 +2,14 @@ import CardImage from './CardImage';
 import CardTitle from './CardTitle';
 import CardDescription from './CardDescription';
 
-const Card = ({ tag: Tag = 'div', children }) => {
+const Card = ({ tag: Tag = 'div', className = '', children }) => {
   return (
-    <Tag className="flex flex-col justify-between flex-1 py-8 px-6 mr-8 bg-white rounded-lg shadow last:mr-0">
+    <Tag
+      className={
+        'max-w-xl flex flex-col justify-between flex-1 py-8 px-6 mr-8 bg-white rounded-lg shadow last:mr-0 ' +
+        className
+      }
+    >
       {children}
     </Tag>
   );
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,40 +1,45 @@
+import Card, { CardTitle } from './Card';
+
 const skills = [
   {
     title: 'Frontend',
     items: [
-      'HTML, CSS, JS',
-      'Pug, SASS (SCSS)',
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'Pug',
+      'SASS (SCSS)',
       'TypeScript',
-      'React, Redux + thunk',
+      'React',
+      'Redux',
       'Tailwind CSS',
       'Material UI',
       'Bootstrap',
-      'Webpack, Gulp',
+      'Webpack',
+      'Gulp',
     ],
   },
   {
     title: 'Backend',
-    items: ['Node.js', 'Express', 'PHP', 'MongoDB', 'PostgreSQL, MySQL'],
+    items: ['Node.js', 'Express', 'PHP', 'MongoDB', 'PostgreSQL', 'MySQL'],
   },
 ];
 
 const Skills = () => {
   return (
-    <section id="skills">
+    <section className="bg-slate-100" id="skills">
       <div className="h-screen container py-20 flex flex-col justify-center items-center">
         <h1 className="text-4xl text-blue-400 mb-10">Skills</h1>
         <ul className="flex justify-center w-full">
           {skills.map((skill) => (
-            <li className="flex-1 py-8 px-6 mr-8 bg-slate-900 rounded-lg last:mr-0">
-              <h2 className="text-3xl text-center text-blue-400 mb-2">
-                {skill.title}
-              </h2>
-              <ul className="flex flex-wrap text-xl list-disc pl-12 pr-4">
+            <Card tag="li">
+              <CardTitle>{skill.title}</CardTitle>
+              <ul className="flex flex-wrap text-xl list-disc pl-12 pr-4 mb-auto">
                 {skill.items.map((item) => (
                   <li className="w-1/2 p-2">{item}</li>
                 ))}
               </ul>
-            </li>
+            </Card>
           ))}
         </ul>
       </div>
