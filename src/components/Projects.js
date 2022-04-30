c07e24ab9ffa4997c4fb8bfee4d5446c81c08e20
yap8 <nebozhenko.arseny@gmail.com>
2022-05-01 00:06:11 +0300

    finished Projects responsive

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -32,10 +32,10 @@ const Projects = () => {
     <section className="bg-slate-100" id="projects">
       <div className="min-h-screen container py-20 flex flex-col justify-center items-center">
         <h1 className="text-4xl text-blue-400 mb-10">Projects</h1>
-        <ul className="flex justify-center w-full flex-wrap">
+        <ul className="flex justify-center w-full flex-wrap lg:justify-around 2xl:justify-between">
           {projects.map((project) => (
             <Card
-              className="mb-8 mr-8 last:mr-0 last:mb-0 lg:max-w-xl xl:mb-0"
+              className="w-full mb-8 last:mb-0 lg:max-w-md 2xl:m-0"
               tag="li"
             >
               <CardImage src={project.image} alt={project.title} />
