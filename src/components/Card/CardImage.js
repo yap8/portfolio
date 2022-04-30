2f1959adcb7c3629b86b07f99f0e78b5d28b5b4b
yap8 <nebozhenko.arseny@gmail.com>
2022-05-01 00:07:13 +0300

    made CardImage change height on lg

--- src/components/Card/CardImage.js
+++ src/components/Card/CardImage.js
@@ -1,6 +1,6 @@
 const CardImage = ({ src, alt }) => {
   return (
-    <div className="h-80 overflow-hidden rounded-lg mb-4 border relative">
+    <div className="h-60 lg:h-80 overflow-hidden rounded-lg mb-4 border relative">
       <img
         className="h-full w-full object-cover absolute"
         src={src}
