25612ebc203ddad7003106d79e149e36ffa3c743
yap8 <nebozhenko.arseny@gmail.com>
2022-04-30 23:43:06 +0300

    fixed last item margin

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -6,7 +6,7 @@ const Card = ({ tag: Tag = 'div', className = '', children }) => {
   return (
     <Tag
       className={
-        'flex flex-col justify-between flex-1 py-8 px-6 mr-8 bg-white rounded-lg shadow ' +
+        'flex flex-col justify-between flex-1 py-8 px-6 mr-8 bg-white rounded-lg shadow last:mr-0 ' +
         className
       }
     >
